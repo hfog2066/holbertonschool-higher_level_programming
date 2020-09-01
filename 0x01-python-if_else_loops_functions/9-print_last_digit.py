6fdd9f80f44c4cca02c3a98011290133e951f1f1
hfog2066 <hfog2000@yahoo.com>
2020-09-01 14:13:26 -0500

    9. There are only 3 colors, 10 digits...

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        l_digit = number % 10
-    else:
-        l_digit = number % -10
-        l_digit *= -1
-
-        print("{:d}".format(l_digit), end='')
-        return (l_digit)
+    number = abs(number) % 10
+    print(number, end='')
+    return(number)
