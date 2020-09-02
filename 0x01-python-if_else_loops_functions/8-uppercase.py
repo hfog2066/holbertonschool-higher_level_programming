f707f493f787bf5c599c6d291fc1f9dcdcdef43b
hfog2066 <hfog2000@yahoo.com>
2020-09-01 19:32:05 -0500

    8. To uppercase.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     print("".format()
-          .join(list(map(lambda x: chr(ord(x) -32)
+          .join(list(map(lambda x: chr(ord(x) - 32)
                          if ord(x) in range(97, 123) else x, str))))
