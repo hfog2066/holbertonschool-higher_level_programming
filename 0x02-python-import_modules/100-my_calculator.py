1d4d6042e7abf6a372684bcf151df57261a4e76d
hfog2066 <hfog2000@yahoo.com>
2020-09-03 13:21:42 -0500

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     if arg_len != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-        if args[2] not in "+-*/":
-            print("Unknow operator. Aviable operators: +, -, * and /")
-            sys.exit(1)
-            a, op, b = int(args[1]), args[2], int(args[3])
-            c = table[args[2]](a, b)
-            print("{} {} {} = {}".format(a, op, b, c))
+    if args[2] not in "+-*/":
+        print("Unknow operator. Aviable operators: +, -, * and /")
+        sys.exit(1)
+    a, op, b = int(args[1]), args[2], int(args[3])
+    c = table[args[2]](a, b)
+    print("{} {} {} = {}".format(a, op, b, c))
