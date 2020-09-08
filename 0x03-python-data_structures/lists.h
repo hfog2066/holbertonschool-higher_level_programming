56617210eb1749a940ff523044f2435392f40be5
hfog2066 <hfog2000@yahoo.com>
2020-09-08 11:20:10 -0500

    lists.h prototypes.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -20,6 +20,10 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+size_t listint_len(const listint_t *h);
+void reverse_linked_list(listint_t **head);
+int compare_linked_list(listint_t *head1, listint_t *head2);
 
 int is_palindrome(listint_t **head);
 
