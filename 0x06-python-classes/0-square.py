c7e95c1468911a90d66a7b4da275d30528904142
hfog2066 <hfog2000@yahoo.com>
2020-09-22 18:48:54 -0500

    0. My first square.

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Square Module - for all your needs, as long as they include printing squares!
+"""
+
+
+class Square:
+    """
+    First pass at creating a square class!
+    """
+    pass
