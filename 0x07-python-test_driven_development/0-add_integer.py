fac21d7043fe7accf56ab58640d8c331110f517c
hfog2066 <hfog2000@yahoo.com>
2020-09-25 19:50:14 -0500

    0. Integers addition.

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if not isinstance(a,(int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b,(int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
