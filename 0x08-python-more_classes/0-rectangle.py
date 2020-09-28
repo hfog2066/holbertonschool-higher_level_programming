064ce269469062b6403ae5dc6fe6149e09979373
hfog2066 <hfog2000@yahoo.com>
2020-09-28 18:40:02 -0500

    0. Simple rectangle.

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This is a module for a rectangle class.
+"""
+
+
+class Rectangle:
+    """A Rectangle class."""
+
+    def __init__(self):
+        """Initialize class."""
+        pass
