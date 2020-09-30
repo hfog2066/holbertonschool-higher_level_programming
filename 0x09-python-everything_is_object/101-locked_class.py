a5b953976a86867bebe84d03760135af1d213413
hfog2066 <hfog2000@yahoo.com>
2020-09-29 19:39:00 -0500

    31. Low memory cost.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,15 @@
 #!/ust/bin/python3
+"""
+
+This module contains a class avoids
+dynamically attributes
+
+"""
+
+
 class LockedClass:
+    __slots__ = ['first_name']
+
     def __init__(self):
+        """ Init method """
         pass
-    __slots__ = ('first_name')
