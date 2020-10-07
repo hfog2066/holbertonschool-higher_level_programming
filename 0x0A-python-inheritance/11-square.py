57f45493fdb2d9a2908a8f7250ff8fc70cade768
hfog2066 <hfog2000@yahoo.com>
2020-10-06 19:22:03 -0500

    11. Square #2.

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Square data that inherits from Rectangle which inherits
+   from BaseGeometry
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Square data that inherits from rectangle
+       init `size` twice because width a height
+      are same in squares.
+    """
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Method to calcualte area of the square
+        """
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Magic method to print square description
+        """
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
