662e8f311c43ff4e82dd404e9d11f59268bc48a7
hfog2066 <hfog2000@yahoo.com>
2020-10-07 20:19:01 -0500

    11. Student to JSON.

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Module Student class
+"""
+
+
+class Student:
+    """Contains student data
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_jason(self):
+        """Retrieves dictionary of Student
+        """
+        return self.__dict__
