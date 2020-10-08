8bd3184d6af087f5615ac8e7b1fad9e16eebc0a1
hfog2066 <hfog2000@yahoo.com>
2020-10-07 20:03:19 -0500

    2. Read n lines.

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+read_lines
+"""
+import os
+
+
+def read_lines(filename="", nb_lines=0):
+    """Takes str filename to read, and n lines with int nb_lines
+    """
+
+
+    with open(filename, encoding="utf-8") as readFile:
+        lineNum = 0
+        while True:
+            line = readFile.readline()
+            lineNum += 1
+            print(line, end='')
+            if lineNum >= nb_lines and nb_lines > 0:
+                break
+            if not line:
+                break
