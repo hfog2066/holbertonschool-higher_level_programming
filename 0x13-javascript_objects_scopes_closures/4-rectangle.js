505e793d406ebe493bfdd6bace805fa12622ed3d
hfog <hfog2000@yahoo.com>
2021-02-08 18:57:50 -0500

    4. Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w = parseInt(w)) && w > 0 &&
+        (h = parseInt(h)) && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height).split('')
+      .slice(0, -1).join(''));
+  }
+
+  rotate () {
+    this.width += this.height;
+    this.height = this.width - this.height;
+    this.width -= this.height;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
